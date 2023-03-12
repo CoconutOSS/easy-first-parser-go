d94393d3dbdf98478ed65d2d8ec837df5d06b2e7
CoconutOSS <161134419+CoconutOSS@users.noreply.github.com>
2023-03-12 05:56:00 +0000

    change imports

--- action.go
+++ action.go
@@ -5,4 +5,6 @@ type StateAction func(state *State, idx int)
 // AttachLeft は左側の単語を右側の単語の親にします
 func AttachLeft(state *State, idx int) {
 	parent := state.pending[idx]
-	chil
+	child := state.pending[idx+1]
+
+	sta
