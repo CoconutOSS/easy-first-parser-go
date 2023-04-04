00a4dcbd0b4145f8b589793d318af38dd98e851d
CoconutOSS <161134419+CoconutOSS@users.noreply.github.com>
2023-04-04 15:46:26 +0000

    Update class.md

--- action_test.go
+++ action_test.go
@@ -29,3 +29,34 @@ func TestAttachLeft(t *testing.T) {
 	}
 
 	if len(s.pending) != 4 {
+		t.Error("length of pending must be 4")
+	}
+}
+
+func TestAttachRight(t *testing.T) {
+	words := make([]*Word, 0)
+	words = append(words,
+		makeRootWord(),
+		makeWord("ms.", "NNP", 1, 2),
+		makeWord("hang", "NNP", 2, 3),
+		makeWord("plays", "VBZ", 3, 0),
+		makeWord("elianti", "NNP", 4, 3),
+		makeWord(".", ".", 5, 3),
+	)
+	s := NewState(words)
+	AttachRight(s, 3)
+	p, ok := s.arcs[3]
+	if !ok || p != 4 {
+		t.Error("parent's index must be 4")
+	}
+
+	AttachRight(s, 3)
+	p, ok = s.arcs[4]
+	if !ok || p != 5 {
+		t.Error("parent's index must be 5")
+	}
+
+	if len(s.pending) != 4 {
+		t.Error("length of pending must be 4")
+	}
+}
