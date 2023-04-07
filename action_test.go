ea2dc0c9d0047cf87025fcc377008048a08e3792
CoconutOSS <161134419+CoconutOSS@users.noreply.github.com>
2023-04-07 05:36:55 +0000

    Removed packages

--- action_test.go
+++ action_test.go
@@ -60,3 +60,24 @@ func TestAttachRight(t *testing.T) {
 		t.Error("length of pending must be 4")
 	}
 }
+
+func TestAttachLeftAll(t *testing.T) {
+	words := make([]*Word, 0)
+	words = append(words,
+		makeRootWord(),
+		makeWord("ms.", "NNP", 1, 2),
+		makeWord("hang", "NNP", 2, 3),
+		makeWord("plays", "VBZ", 3, 0),
+		makeWord("elianti", "NNP", 4, 3),
+		makeWord(".", ".", 5, 3),
+	)
+	s := NewState(words)
+	AttachLeft(s, 0)
+	AttachLeft(s, 0)
+	AttachLeft(s, 0)
+	AttachLeft(s, 0)
+	AttachLeft(s, 0)
+	if words[1].surface != "ms." {
+		t.Error("surface is wrong")
+	}
+}
