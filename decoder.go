35eb4a9149727747d01d0ba98ae3ef2a6146d81c
CoconutOSS <161134419+CoconutOSS@users.noreply.github.com>
2023-04-07 10:30:26 +0000

    NFSv3: Ensure that do_proc_get_root() reports errors correctly

--- decoder.go
+++ decoder.go
@@ -1,2 +1,26 @@
 
 package main
+
+func isFinished(state *State) bool {
+	return len(state.pending) == 1
+}
+
+func decode(weight *[]float64, state *State) {
+	if isFinished(state) {
+		// Do nothing
+	} else {
+		pair := BestActionIndexPair(weight, state)
+		pair.action(state, pair.index)
+		state.ResetFvCache(pair.index)
+		decode(weight, state)
+	}
+}
+
+func Decode(weight *[]float64, sent *Sentence) {
+	s := NewState(sent.words)
+	decode(weight, s)
+
+	for child, parent := range s.arcs {
+		sent.words[child].predHead = parent
+	}
+}
