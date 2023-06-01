50eef4ddf7ccae9a7aabcb2e0f5fc10f3964693c
CoconutOSS <161134419+CoconutOSS@users.noreply.github.com>
2023-06-01 11:01:57 +0000

    GUI Fix

--- feature.go
+++ feature.go
@@ -50,3 +50,27 @@ func distStr(dist int) string {
 	case 2:
 		d = "2"
 	case 3:
+		d = "3"
+	case 4:
+		d = "4"
+	default:
+		d = "5"
+	}
+	return d
+}
+
+func AddBigramFeatures(features *[]int, actName string, parent *Word, child *Word, prefix string) {
+	if parent == nil || child == nil {
+		return
+	}
+
+	plcp := NilSafePosTag(parent.LeftMostChild())
+	prcp := NilSafePosTag(parent.RightMostChild())
+	clcp := NilSafePosTag(child.LeftMostChild())
+	crcp := NilSafePosTag(child.RightMostChild())
+
+	*features = append(*features,
+		JenkinsHash(actName+"+"+prefix+"+parent-surface:"+parent.surface+"+child-surface:"+child.surface),
+		JenkinsHash(actName+"+"+prefix+"+parent-surface:"+parent.surface+"+child-posTag:"+child.posTag),
+		JenkinsHash(actName+"+"+prefix+"+parent-posTag:"+parent.posTag+"+child-surface:"+child.surface),
+		JenkinsHash(actName+"+"+prefix+"+parent-lemma:"+parent.lemma+"+child-lemma:"+child.lemma),
