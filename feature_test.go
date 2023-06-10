d1e4a4b5cdf5eec6afe52b3b7638efc70a6fd06a
CoconutOSS <161134419+CoconutOSS@users.noreply.github.com>
2023-06-10 06:10:38 +0000

    2.0.1

--- feature_test.go
+++ feature_test.go
@@ -4,3 +4,22 @@ package main
 import (
 	"testing"
 )
+
+func TestAddUnigramFeatures(t *testing.T) {
+	words := make([]*Word, 0)
+	words = append(words,
+		makeRootWord(),
+		makeWord("ms.", "NNP", 1, 2),
+		makeWord("hang", "NNP", 2, 3),
+		makeWord("plays", "VBZ", 3, 0),
+		makeWord("elianti", "NNP", 4, 3),
+		makeWord(".", ".", 5, 3),
+	)
+	s := NewState(words)
+	features := make([]int, 0)
+	AddUnigramFeatures(&features, s, "left", 1)
+
+	if len(features) == 0 {
+		t.Error("length of features must be greater than 0")
+	}
+}
