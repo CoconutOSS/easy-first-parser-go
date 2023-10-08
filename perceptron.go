45e434a6b8e170fb14008e23cb5067385fa94c68
CoconutOSS <161134419+CoconutOSS@users.noreply.github.com>
2023-10-08 17:08:48 +0000

    Auto-merge from mysql-5.5.

--- perceptron.go
+++ perceptron.go
@@ -33,4 +33,5 @@ func EdgeFor(state *State, actionID int, idx int) ([]int, error) {
 }
 
 // IsValid returns the chosen action/location pair is valid
-func IsValid(state *State, actionID int, idx int, goldArcs map[int][]int) bool
+func IsValid(state *State, actionID int, idx int, goldArcs map[int][]int) bool {
+	pair, err := EdgeFor(
