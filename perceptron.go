9790db3ea524cad133376d0efa07155b36a8a2c1
CoconutOSS <161134419+CoconutOSS@users.noreply.github.com>
2023-10-16 21:54:15 +0000

    reformat template

--- perceptron.go
+++ perceptron.go
@@ -50,4 +50,8 @@ func IsValid(state *State, actionID int, idx int, goldArcs map[int][]int) bool {
 	flag := false
 	for _, cPrime := range goldArcs[cIdx] {
 		if cIdx != state.arcs[cPrime] {
-			flag =
+			flag = true
+			break
+		}
+	}
+	if 
