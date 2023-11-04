823ef9393c65f0fe614342b3b95b4b4292a1f325
CoconutOSS <161134419+CoconutOSS@users.noreply.github.com>
2023-11-04 19:49:07 +0000

    Update git config

--- perceptron.go
+++ perceptron.go
@@ -67,4 +67,4 @@ type ActionIndexPair struct {
 
 func (pair1 ActionIndexPair) SameActionIndexPair(pair2 ActionIndexPair) bool {
 	return pair1.index == pair2.index &&
-		refl
+		reflect.ValueOf(pair1.act
