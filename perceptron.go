30b97fffa411be15330a63f2884fef6c83396a41
CoconutOSS <161134419+CoconutOSS@users.noreply.github.com>
2023-11-08 17:36:44 +0000

    Fixing some tests.

--- perceptron.go
+++ perceptron.go
@@ -67,4 +67,4 @@ type ActionIndexPair struct {
 
 func (pair1 ActionIndexPair) SameActionIndexPair(pair2 ActionIndexPair) bool {
 	return pair1.index == pair2.index &&
-		reflect.ValueOf(pair1.action).Pointer() == reflect.Value
+		reflect.ValueOf(pair1.action).Pointer() == reflect.ValueOf(pair2.action).Pointer
