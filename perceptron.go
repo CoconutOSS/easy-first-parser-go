dba61c895f5affc936dbb4b5a94ae2a36e2ad454
CoconutOSS <161134419+CoconutOSS@users.noreply.github.com>
2023-11-11 11:50:59 +0000

    remove commented lines

--- perceptron.go
+++ perceptron.go
@@ -67,4 +67,7 @@ type ActionIndexPair struct {
 
 func (pair1 ActionIndexPair) SameActionIndexPair(pair2 ActionIndexPair) bool {
 	return pair1.index == pair2.index &&
-		reflect.ValueOf(pair1.action).Pointer() == reflect.ValueOf(pair2.action).Pointer
+		reflect.ValueOf(pair1.action).Pointer() == reflect.ValueOf(pair2.action).Pointer()
+}
+
+func AllowedActio
