3a46a1e51d45c39d3425d82f33ba45a0766d7502
CoconutOSS <161134419+CoconutOSS@users.noreply.github.com>
2023-11-28 11:55:28 +0000

    Stubs

--- perceptron.go
+++ perceptron.go
@@ -75,4 +75,4 @@ func AllowedActions(state *State, goldArcs map[int][]int) []ActionIndexPair {
 	for actionID, f := range StateActions {
 		for idx := 0; idx < len(state.pending)-1; idx++ {
 			if IsValid(state, actionID, idx, goldArcs) {
-				result =
+				result = append(result, ActionIn
