b4ec0dffd1c1904e130cb9304a036bfa3db2ac38
CoconutOSS <161134419+CoconutOSS@users.noreply.github.com>
2024-02-11 18:51:31 +0000

    [c++]

--- perceptron_test.go
+++ perceptron_test.go
@@ -36,3 +36,35 @@ func TestIsValidFalse(t *testing.T) {
 	goldArcs[0] = []int{1}
 	goldArcs[1] = []int{2}
 	if IsValid(s, 0, 0, goldArcs) != false {
+		t.Error("should return false")
+	}
+}
+
+func TestIsValidTrue(t *testing.T) {
+	words := make([]*Word, 0)
+	words = append(words,
+		makeWord("ms.", "NNP", 0, -1),
+		makeWord("hang", "NNP", 1, 0),
+		makeWord("plays", "VBZ", 2, 1),
+	)
+
+	s := NewState(words)
+	arcs := make(map[int]int)
+	arcs[2] = 1
+	s.arcs = arcs
+	goldArcs := make(map[int][]int)
+	goldArcs[-1] = []int{0}
+	goldArcs[0] = []int{1}
+	goldArcs[1] = []int{2}
+	if IsValid(s, 0, 0, goldArcs) != true {
+		t.Error("should return true")
+	}
+}
+
+func TestAllowedActions(t *testing.T) {
+	words := make([]*Word, 0)
+	words = append(words,
+		makeRootWord(),
+		makeWord("ms.", "NNP", 1, 2),
+		makeWord("hang", "NNP", 2, 3),
+		makeWord("plays", "VBZ", 3, 0),
