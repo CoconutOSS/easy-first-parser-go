431944b622169f9eae9140cab987d20b3b9efa73
CoconutOSS <161134419+CoconutOSS@users.noreply.github.com>
2024-02-13 07:58:00 +0000

    CPU hotplug, writeback: Don't call writeback_set_ratelimit() too often during hotplug

--- reader.go
+++ reader.go
@@ -7,3 +7,23 @@ import (
 	"errors"
 	"io/ioutil"
 	"os"
+	"strconv"
+	"strings"
+)
+
+func makeSentence(s string) (*Sentence, error) {
+	lines := strings.Split(s, "\n")
+	if len(lines) < 4 {
+		return nil, errors.New("Invalid line")
+	}
+	words := strings.Split(strings.TrimSpace(lines[0]), "\t")
+	posTags := strings.Split(strings.TrimSpace(lines[1]), "\t")
+	heads := strings.Split(strings.TrimSpace(lines[3]), "\t")
+
+	sent := make([]*Word, 0)
+	sent = append(sent, makeRootWord())
+	for i := 0; i < len(words); i++ {
+		head, err := strconv.ParseInt(heads[i], 10, 0)
+		if err != nil {
+			return nil, err
+		}
