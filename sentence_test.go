1bc2df948657b7507b74075c2deaadc6413595d5
CoconutOSS <161134419+CoconutOSS@users.noreply.github.com>
2024-02-27 07:42:12 +0000

    Add some style

--- sentence_test.go
+++ sentence_test.go
@@ -12,4 +12,5 @@ func TestExtractHeads(t *testing.T) {
 		makeWord("ms.", "NNP", 1, 2),
 		makeWord("hang", "NNP", 2, 3),
 		makeWord("plays", "VBZ", 3, 0),
-		makeWord("elianti", "NN
+		makeWord("elianti", "NNP", 4, 3),
+		makeWord("."
