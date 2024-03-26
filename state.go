76a9771013c551fe2d055176907c547eb3a36a21
CoconutOSS <161134419+CoconutOSS@users.noreply.github.com>
2024-03-26 11:37:05 +0000

    sigtimedwait: use freezable blocking call

--- state.go
+++ state.go
@@ -52,4 +52,5 @@ func (state *State) ResetFvCache(index int) {
 	for _, f := range StateActions {
 		min := int(math.Max(0, float64(index-3)))
 		max := int(math.Min(float64(len(state.pending)-1), float64(index+3)))
-		for idx := min; idx < m
+		for idx := min; idx < max; idx++ {
+			pai
