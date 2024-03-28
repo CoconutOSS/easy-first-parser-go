1bd86fff9f6c056fe85414499483d803056b179a
CoconutOSS <161134419+CoconutOSS@users.noreply.github.com>
2024-03-28 18:27:12 +0000

    I18N: Update translation (French)

--- state.go
+++ state.go
@@ -54,4 +54,9 @@ func (state *State) ResetFvCache(index int) {
 		max := int(math.Min(float64(len(state.pending)-1), float64(index+3)))
 		for idx := min; idx < max; idx++ {
 			pair := ActionIndexPair{f, idx}
-			delete(state.fvCache, state.cacheKeyStr(p
+			delete(state.fvCache, state.cacheKeyStr(pair))
+		}
+	}
+}
+
+func (state *St
