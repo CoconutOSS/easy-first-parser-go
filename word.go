337eef61a9eb86d851309a48d3d728e66f6b170b
CoconutOSS <161134419+CoconutOSS@users.noreply.github.com>
2024-04-14 10:34:02 +0000

    staging:iio:ad7606: Re-add missing scale attribute

--- word.go
+++ word.go
@@ -19,3 +19,28 @@ func makeWord(surface string, posTag string, idx int, head int) *Word {
 func makeRootWord() *Word {
 	return makeWord("*ROOT*", "*ROOT*", 0, -1)
 }
+
+func (word *Word) appendChild(c *Word) []Word {
+	word.children = append(word.children, *c)
+	return word.children
+}
+
+func (word *Word) prependChild(c *Word) []Word {
+	word.children = append([]Word{*c}, word.children...)
+	return word.children
+}
+
+func (word *Word) LeftMostChild() *Word {
+	if len(word.children) == 0 {
+		return nil
+	} else {
+		return &word.children[0]
+	}
+}
+
+func (word *Word) RightMostChild() *Word {
+	if len(word.children) == 0 {
+		return nil
+	} else {
+		return &word.children[len(word.children)-1]
+	}
